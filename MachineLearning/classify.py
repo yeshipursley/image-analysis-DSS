79624004d9375b7dace7a3870f915f3331966fd1
Håvard Fjørkenstad <haavarfj@stud.ntnu.no>
2022-03-07 16:55:24 +0100

    Fixed model strucutre

--- MachineLearning/classify.py
+++ MachineLearning/classify.py
@@ -31,31 +31,31 @@ class Classifier():
 
         return topPrediction
 
+# It is important that the model used is trained on the same structure as this one
 class Convolutional(nn.Module):
     def __init__(self):
         super(Convolutional, self).__init__()
-        self.conv1 = nn.Conv2d(1, 6, 5) # 1 input image channel, 6 output channels, 5x5 square convolution
-        self.pool = nn.MaxPool2d(2,2)
-        self.conv2 = nn.Conv2d(6, 16, 5) # 6 input image channel, 16 output channels, 5x5 square convolution
-        self.fc1 = nn.Linear(16 * 5 * 5, 256) 
-        self.fc2 = nn.Linear(256, 128)
-        self.fc3 = nn.Linear(128, 10)
-
+        # Convolutional layers and Max pooling with activation functions
+        self.convolutional = nn.Sequential(
+            nn.Conv2d(1, 6, 5), 
+            nn.ReLU(),
+            nn.MaxPool2d(2,2),
+            nn.Conv2d(6, 16, 5), 
+            nn.ReLU(),
+            nn.MaxPool2d(2,2)
+        )
+        
+        # Fully connected layer with activation functions
+        self.fullyconnected = nn.Sequential(
+            nn.Linear(16 * 5 * 5, 256),
+            nn.ReLU(),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Linear(128, 22)
+        )
 
     def forward(self, x):
-        # First convolution and pooling
-        x = F.relu(self.conv1(x))
-        x = self.pool(x)
-
-        # Second convolution and pooling
-        x = F.relu(self.conv2(x))
-        x = self.pool(x)
-
-        # Flatten dimensions
+        x = self.convolutional(x)
         x = torch.flatten(x, 1)
-
-        # Fully connected layers
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.fullyconnected(x)
         return x
