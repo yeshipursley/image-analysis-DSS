3b1c75701b5091716a80aaf4c384ab2e13020bf5
yeshi <yeshijp@stud.ntnu.no>
2022-03-14 13:08:49 +0100

    Created a letter object. Saves the letter object into an array.

--- ImageSegmentation/Segmentation-To-Classifier/segmentation_to_classifier.py
+++ ImageSegmentation/Segmentation-To-Classifier/segmentation_to_classifier.py
@@ -1,156 +1,91 @@
 # OpenCV
 import cv2
+
 import pytesseract
+
+# For changing the directory so that we can save images in a different folder
 import os
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
 
-import numpy as np
+# Necessary for running pytesseract
+# Info on how to get it running: https://github.com/tesseract-ocr/tesseract/blob/main/README.md
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-class MetaImage():
-    def __init__(self):
-        # setup goes here
-        return
-    
-    def AddLabel(self, label):
-        # label function
-        return 
+# Reads image of scroll
+img = cv2.imread('PLACEHOLDER')
 
-class Segmentor():
-    def __init__(self):
-        return
-    
-    def Segment(self, image):
-        # Necessary for running pytesseract
-        # Info on how to get it running: https://github.com/tesseract-ocr/tesseract/blob/main/README.md
-        pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+# Grayscales image
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        # Reads image of scroll
-        img = image
+# otsu thresholding
+_, otsu = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 
-        # Grayscales image
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Denoises the otsu image
+deNoiseOtsu = cv2.fastNlMeansDenoising(otsu, h=60.0, templateWindowSize=7, searchWindowSize=21)
 
-        # otsu thresholding
-        _, otsu = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+# Change the current directory
+# to specified directory
+# ADD desired folder for segmented letters/words
+os.chdir('PLACEHOLDER')
 
-        # Denoises the otsu image
-        deNoiseOtsu = cv2.fastNlMeansDenoising(otsu, h=60.0, templateWindowSize=7, searchWindowSize=21)
+# Incremented ID for cropped images
+id = 0
 
-        # Change the current directory
-        # to specified directory
-        # ADD desired folder for segmented letters/words
-        os.chdir('PLACEHOLDER')
+## Crops the images around the letters/words
+# Saves the height and width of the images
+hImg, wImg = deNoiseOtsu.shape
 
-        # Incremented ID for cropped images
-        id = 0
 
-        ## Crops the images around the letters/words
-        # Saves the height and width of the images
-        hImg, wImg = deNoiseOtsu.shape
+# Object for letters that contain the image, the coordinates, and the classification.
+class Letter:
+    def __init__(self, image, x, y, w, h, classification):
+        self.image = image
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.classification = classification
 
-        # array for the segmented letters
-        segmentedLetters = []
 
-        # Makes a box around each letter/word on the scroll
-        boxes = pytesseract.image_to_boxes(deNoiseOtsu, lang="heb")
+# array for the segmented letters
+segmentedLetters = []
 
-        # For each box
-        for b in boxes.splitlines():
-            # Splits the values of the box into an array
-            b = b.split(' ')
-            # Save the coordinates of the box:
-            # x = Distance between the top left corner of the box to the left frame
-            # y = Distance between the top of the box to the bottom frame
-            # w = Distance between the right side of the box to the left frame
-            # h = Distance between the bottom of the box to the bottom frame
-            x, y, w, h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
+# Makes a box around each letter/word on the scroll
+boxes = pytesseract.image_to_boxes(deNoiseOtsu, lang="heb")
 
-            # Crop the image so that we only get the letter/word
-            # Structure image[rows, col]
-            crop = otsu[(hImg - h):(hImg - y), x:w]
+# For each box
+for b in boxes.splitlines():
+    # Splits the values of the box into an array
+    b = b.split(' ')
+    # Save the coordinates of the box:
+    # x = Distance between the top left corner of the box to the left frame
+    # y = Distance between the top of the box to the bottom frame
+    # w = Distance between the right side of the box to the left frame
+    # h = Distance between the bottom of the box to the bottom frame
+    x, y, w, h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
 
-            # Height and width of the cropped image
-            hBox, wBox = crop.shape
+    # Crop the image so that we only get the letter/word
+    # Structure image[rows, col]
+    crop = otsu[(hImg - h):(hImg - y), x:w]
 
-            # Checks if the crop is too small or too large
-            if hBox > (1 / hBox * 100) and wBox > (1 / hBox * 100):
-                # appends the crop to the array
-                segmentedLetters.append(crop)
-                print("Successfully appended letter")
+    # Height and width of the cropped image
+    hBox, wBox = crop.shape
 
-                # Creates a blue rectangle over the letter/image
-                cv2.rectangle(img, (x, hImg - y), (w, hImg - h), (255, 0, 0), 1)
-            else:
-                print("Image to small or to large.")
+    # Checks if the crop is too small or too large
+    if hBox > (1 / hBox * 100) and wBox > (1 / hBox * 100):
+        # Saves the cropped letter as an object
+        croppedLetter = Letter(crop, x, y, w, h, None)
 
-                # Creates a red rectangle over it
-                cv2.rectangle(img, (x, hImg - y), (w, hImg - h), (0, 0, 255), 1)
+        # appends the cropped letter to the array
+        segmentedLetters.append(croppedLetter)
+        print("Successfully appended letter")
 
-        # Saves the image with all the rectangles
-        print("Saving the img was successful:", cv2.imwrite('segImage.png', img))
-        return segmentedLetters
-        
+        # Creates a blue rectangle over the letter/image
+        cv2.rectangle(img, (x, hImg - y), (w, hImg - h), (255, 0, 0), 1)
+    else:
+        print("Image to small or to large.")
 
-class Classifier():
-    def __init__(self, model):
+        # Creates a red rectangle over it
+        cv2.rectangle(img, (x, hImg - y), (w, hImg - h), (0, 0, 255), 1)
 
-        # Setup model
-        self.model = Convolutional()
-        self.model.load_state_dict(torch.load(model))
-        self.model.eval()
-
-        # Setup classes
-        self._classes = ['ALEF', 'BET', 'GIMEL', 'DALET', 'HE', 'VAV', 'ZAYIN', 'HET', 'TET', 'YOD', 'KAF', 'LAMED', 'MEM', 'NUN', 'SAMEKH', 'AYIN', 'PE', 'TSADI', 'QOF', 'RESH', 'SHIN', 'TAV']
-    
-    def Classify(self, images):
-        # Ensure that the array is a numpy array
-        images = np.asarray(images)
-
-        # Convert the numpy arrays into tensors
-        images = torch.from_numpy(images).float()
-    
-        # Fix the shape of the array
-        images = images.unsqueeze(1)
-
-        # Predict
-        confidenseValues = self.model(images)
-
-        # Convert the predictions to a numpy array
-        confidenseValues = confidenseValues.detach().numpy()
-        # Get the highest confidense value
-        predictions = list()
-        for confidenseValue in confidenseValues:
-            highestConfidense = np.argmax(confidenseValue)
-            predictions.append(self._classes[highestConfidense])
-
-        return predictions
-
-class Convolutional(nn.Module):
-    def __init__(self):
-        super(Convolutional, self).__init__()
-        # Convolutional layers and Max pooling with activation functions
-        self.convolutional = nn.Sequential(
-            nn.Conv2d(1, 6, 5), 
-            nn.ReLU(),
-            nn.MaxPool2d(2,2),
-            nn.Conv2d(6, 16, 5), 
-            nn.ReLU(),
-            nn.MaxPool2d(2,2)
-        )
-        
-        # Fully connected layer with activation functions
-        self.fullyconnected = nn.Sequential(
-            nn.Linear(16 * 5 * 5, 256),
-            nn.ReLU(),
-            nn.Linear(256, 128),
-            nn.ReLU(),
-            nn.Linear(128, 22)
-        )
-
-    def forward(self, x):
-        x = self.convolutional(x)
-        x = torch.flatten(x, 1)
-        x = self.fullyconnected(x)
-        return x
+# Saves the image with all the rectangles
+print("Saving the img was successful:", cv2.imwrite('segImage.png', img))
